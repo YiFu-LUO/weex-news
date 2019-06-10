74281c48818e160b03c845963f4ff145edd1303b
lyf <2978461417@qq.com>
2019-06-11 00:58:19 +0800

    初始化

---
+++ dist/components/tabbar.js
@@ -0,0 +1,329 @@
+// { "framework": "Vue"} 
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 12);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */,
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __vue_exports__, __vue_options__
+var __vue_styles__ = []
+
+/* styles */
+__vue_styles__.push(__webpack_require__(3)
+)
+
+/* script */
+__vue_exports__ = __webpack_require__(4)
+
+/* template */
+var __vue_template__ = __webpack_require__(5)
+__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+if (
+  typeof __vue_exports__.default === "object" ||
+  typeof __vue_exports__.default === "function"
+) {
+if (Object.keys(__vue_exports__).some(function (key) { return key !== "default" && key !== "__esModule" })) {console.error("named exports are not supported in *.vue files.")}
+__vue_options__ = __vue_exports__ = __vue_exports__.default
+}
+if (typeof __vue_options__ === "function") {
+  __vue_options__ = __vue_options__.options
+}
+__vue_options__.__file = "E:\\webwork\\weex\\awesome-project\\src\\components\\tabbar.vue"
+__vue_options__.render = __vue_template__.render
+__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+__vue_options__._scopeId = "data-v-533c8564"
+__vue_options__.style = __vue_options__.style || {}
+__vue_styles__.forEach(function (module) {
+  for (var name in module) {
+    __vue_options__.style[name] = module[name]
+  }
+})
+if (typeof __register_static_styles__ === "function") {
+  __register_static_styles__(__vue_options__._scopeId, __vue_styles__)
+}
+
+module.exports = __vue_exports__
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = {
+  "wrap": {
+    "height": "90",
+    "width": "750",
+    "flexDirection": "row",
+    "fontSize": "30",
+    "backgroundColor": "#ffffff"
+  },
+  "tab-bar": {
+    "justifyContent": "space-between",
+    "paddingLeft": "30",
+    "paddingRight": "30"
+  },
+  "scroll": {
+    "height": "90",
+    "width": "750",
+    "flexDirection": "row"
+  },
+  "tab-bar-item": {
+    "height": "90",
+    "flexDirection": "column",
+    "alignItems": "center",
+    "justifyContent": "center"
+  },
+  "tab-bar-scroll-width": {
+    "width": "150"
+  },
+  "tab-bar-title": {
+    "height": "90",
+    "lineHeight": "90",
+    "fontSize": "30",
+    "color": "#303133",
+    "borderBottomWidth": "4",
+    "borderColor": "rgba(0,0,0,0)"
+  },
+  "active": {
+    "color": "#ec706b",
+    "borderColor": "#ec706b"
+  }
+}
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+var dom = weex.requireModule('dom');
+
+exports.default = {
+	props: {
+		drag: {
+			type: Boolean,
+			default: true
+		},
+		tabBars: {
+			type: Array,
+			default: function _default(e) {
+				return [];
+			}
+		},
+		tabIndex: {
+			type: Number,
+			default: 0
+		}
+	},
+	watch: {
+		tabIndex: function tabIndex(newVal) {
+			this.change(newVal);
+		}
+	},
+	methods: {
+		change: function change(index, e) {
+
+			var ret = {
+				index: index
+			};
+
+			this.$emit('tabChange', ret);
+			// const el = this.$refs[this.tabBars[index].id + index][0]
+			// if (elSize.left + elSize.width > 750) {
+			//     let idx = index - 4;
+			//     let newEl = this.$refs[this.tabBars[idx].id + idx][0]
+			//     dom.scrollToElement(newEl, {});
+			//     return;
+			// }
+			// if (elSize.left < 0) {
+			//     dom.scrollToElement(el, {});
+			// }
+		},
+		getElSize: function getElSize(el) {
+			//得到元素的size
+			return new Promise(function (res, rej) {
+				var result = dom.getComponentRect(el, function (option) {
+					res(option.size);
+				});
+			});
+		}
+	}
+};
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', [(_vm.drag) ? _c('div', {
+    staticClass: ["wrap", "tab-bar-scroll"]
+  }, [_c('scroller', {
+    staticClass: ["scroll"],
+    attrs: {
+      "scrollDirection": "horizontal",
+      "showScrollbar": "false"
+    }
+  }, _vm._l((_vm.tabBars), function(tabBar, t) {
+    return _c('div', {
+      key: t,
+      ref: tabBar.id + t,
+      refInFor: true,
+      staticClass: ["tab-bar-item", "tab-bar-scroll-width"],
+      on: {
+        "click": function($event) {
+          _vm.change(t)
+        }
+      }
+    }, [_c('text', {
+      staticClass: ["tab-bar-title"],
+      class: [_vm.tabIndex === t ? 'active' : '']
+    }, [_vm._v(_vm._s(tabBar.name))])])
+  }))]) : _c('div', {
+    staticClass: ["wrap", "tab-bar"]
+  }, _vm._l((_vm.tabBars), function(tabBar, t) {
+    return _c('div', {
+      key: t,
+      ref: tabBar.id + t,
+      refInFor: true,
+      staticClass: ["tab-bar-item"],
+      on: {
+        "click": function($event) {
+          _vm.change(t)
+        }
+      }
+    }, [_c('text', {
+      staticClass: ["tab-bar-title"],
+      class: [_vm.tabIndex === t ? 'active' : '']
+    }, [_vm._v(_vm._s(tabBar.name))])])
+  }))])
+},staticRenderFns: []}
+module.exports.render._withStripped = true
+
+/***/ }),
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */,
+/* 11 */,
+/* 12 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _tabbar = __webpack_require__(2);
+
+var _tabbar2 = _interopRequireDefault(_tabbar);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_tabbar2.default.el = '#root';
+new Vue(_tabbar2.default);
+
+/***/ })
+/******/ ]);
